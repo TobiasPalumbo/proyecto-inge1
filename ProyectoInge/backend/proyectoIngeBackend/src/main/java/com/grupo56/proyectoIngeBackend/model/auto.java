2e8768604edab2e4a628560528eaeacdf52052de
Carlos Benitez <carlos_andres01.10@hotmail.com>
2025-04-20 14:27:23 -0300

    Pruebas de back y aprendiendo, manejo del back junto a la db

--- ProyectoInge/backend/proyectoIngeBackend/src/main/java/com/grupo56/proyectoIngeBackend/model/auto.java
+++ ProyectoInge/backend/proyectoIngeBackend/src/main/java/com/grupo56/proyectoIngeBackend/model/auto.java
@@ -14,6 +14,10 @@ public class auto {
 	public auto() {
 		super();
 	}
+	 public int getId() { return id; }
+	    public void setId(int id) { this.id = id; }
 
+	    public String getNom() { return nom; }
+	    public void setNom(String nom) { this.nom = nom; }
 	
 }
--- ProyectoInge/backend/proyectoIngeBackend/src/main/java/com/grupo56/proyectoIngeBackend/service/autoserviceimp.java
+++ ProyectoInge/backend/proyectoIngeBackend/src/main/java/com/grupo56/proyectoIngeBackend/service/autoserviceimp.java
@@ -1,5 +1,32 @@
 package com.grupo56.proyectoIngeBackend.service;
 
-public class autoserviceimp {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.grupo56.proyectoIngeBackend.model.auto;
+import com.grupo56.proyectoIngeBackend.repository.autorepo;
+//aca esta la logica de la entidad, metodos llamados desde el controller 
+@Service
+public class autoserviceimp {
+	@Autowired
+	private autorepo autoRepository;
+	
+	public List<auto> obtenerAutos(){
+		return autoRepository.findAll();
+	}
+	public auto cargarAuto(auto car) {
+		return autoRepository.save(car);
+	}
+	public void borrarAuto( int id) {
+		autoRepository.deleteById(id);
+	}
+	public void borrarTodo() {
+		autoRepository.deleteAll();
+	}
+	public void reiniciarIdCount() {
+		autoRepository.resetAutoIncrement();
+	}
+	
 }
