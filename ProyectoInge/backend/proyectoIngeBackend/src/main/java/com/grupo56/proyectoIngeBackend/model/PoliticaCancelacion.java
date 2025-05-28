682edd47092712199948d9d4ceda229c4d40f624
Carlos Benitez <carlos_andres01.10@hotmail.com>
2025-05-27 21:31:34 -0300

    ultimo Push Antes de arreglar todo, para merguear en una rama conjunta back-front

--- ProyectoInge/backend/proyectoIngeBackend/src/main/java/com/grupo56/proyectoIngeBackend/model/PoliticaCancelacion.java
+++ ProyectoInge/backend/proyectoIngeBackend/src/main/java/com/grupo56/proyectoIngeBackend/model/PoliticaCancelacion.java
@@ -13,7 +13,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PoliticaCancelacion {
-		
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idPoliticaCancelacion;
